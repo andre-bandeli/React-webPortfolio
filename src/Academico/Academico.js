93f76e9352839f57fc55b717d20586014b4277b9
andre-bandeli <andre.lbandeli@gmail.com>
2021-11-03 11:58:26 -0300

    --- Auto-Git Commit ---

--- src/Academico/Academico.js
+++ src/Academico/Academico.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navbar from '../Home/components/Navbar/Navbar';
 import './academico.scss';
-import Container from './components/container/Container';
 
 const Academico = () => {
     return (
         <div className='academico'>
             <Navbar/>
-            <Container/>
         </div>
     );
 }
--- src/Routes.js
+++ src/Routes.js
@@ -4,7 +4,6 @@ import { Route, BrowserRouter } from "react-router-dom";
 import Home from "./Home/Home";
 import Sobre from "./Sobre/Sobre";
 import Usuario from "./Usuario/Usuario";
-import Academico from "./Academico/Academico";
 
 const Routes = () => {
    return(
@@ -12,7 +11,6 @@ const Routes = () => {
            <Route component = { Home }  path="/" exact />
            <Route component = { Sobre }  path="/sobre" />
            <Route component = { Usuario }  path="/usuario" />
-           <Route component = { Academico }  path="/academico" />
        </BrowserRouter>
    )
 }
